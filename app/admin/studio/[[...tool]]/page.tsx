a4bcabf5851b210299a6d5689763f76fc4afff53
najmanmna <139351540+najmanmna@users.noreply.github.com>
2026-01-13 14:40:04 +0530

    sanity admin page changed

--- app/admin/studio/[[...tool]]/page.tsx
+++ app/admin/studio/[[...tool]]/page.tsx
@@ -1,10 +1,17 @@
 "use client";
 
-// 👇 ADD THIS LINE
-export const dynamic = "force-static";
-
 import dynamic from "next/dynamic";
 import config from "../../../../sanity.config";
+
+// 1. Force Static Mode
+export const dynamicParams = true; 
+
+// 2. This is the Magic Fix:
+// By returning an empty array, we tell Next.js to pre-build this page.
+// This moves it from the "3MB Function Limit" to the "Unlimited Asset Limit".
+export function generateStaticParams() {
+  return [];
+}
 
 // Dynamically import NextStudio so it runs only in browser
 const NextStudio = dynamic(
