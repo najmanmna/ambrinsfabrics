294c567819d87234cfea681ec4f436c6f7a45334
najmanmna <139351540+najmanmna@users.noreply.github.com>
2026-01-13 13:56:19 +0530

    uncommented apis

--- app/api/voucher/route.ts
+++ app/api/voucher/route.ts
@@ -1,17 +1,17 @@
-// import { NextResponse } from "next/server";
-// import { client } from "@/sanity/lib/client";
+import { NextResponse } from "next/server";
+import { client } from "@/sanity/lib/client";
 export const runtime = 'edge';
 
-// export async function GET() {
-//   // Updated query to order by the creation timestamp (newest first)
-//   const query = `*[_type == "voucherTemplate"] | order(_createdAt desc) {
-//     _id,
-//     title,
-//     amount,
-//     description,
-//     image { asset->{url} }
-//   }`;
+export async function GET() {
+  // Updated query to order by the creation timestamp (newest first)
+  const query = `*[_type == "voucherTemplate"] | order(_createdAt desc) {
+    _id,
+    title,
+    amount,
+    description,
+    image { asset->{url} }
+  }`;
 
-//   const vouchers = await client.fetch(query);
-//   return NextResponse.json(vouchers);
-// }
+  const vouchers = await client.fetch(query);
+  return NextResponse.json(vouchers);
+}
